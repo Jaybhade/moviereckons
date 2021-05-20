aa40e14c7a11043eaa9f443aea09a2eaa9978174
jaybhade <jayeshjibhade@gmail.com>
2021-05-20 22:48:07 +0530

    Added Cards

--- components/navbar/brand.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Image from "next/image";
-
-const Brand = () => {
-  return <Image src="/logo.png" alt="Company Logo" width={70} height={50} />;
-};
-
-export default Brand;
--- pages/_app.js
+++ pages/_app.js
@@ -4,38 +4,9 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import LeftPanel from "../components/leftpanel";
 import { Tablet, Mobile, Desktop } from "../lib/responsive";
-import Navbar from "../components/navbar/navbar";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');;
-
-  *,
-  *::after,
-  *::before {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: inherit;
-  }
-
-  html {
-    font-size: 62.5%;
-  }
-
-  body {
-    box-sizing: border-box;
-    font-family: 'Montserrat', sans-serif;
-  }
-`;
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [navbarOpen, setNavbarOpen] = useState(false);
-
-  const handleNavbar = () => {
-    setNavbarOpen(!navbarOpen);
-  };
 
   return (
     <div className="body">
@@ -54,14 +25,6 @@ function MyApp({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
       </Head>
-      {/* <Mobile>
-        <Navbar navbarState={navbarOpen} handleNavbar={handleNavbar} />
-        <GlobalStyle />
-      </Mobile> */}
-      {/* <Tablet>
-        <Navbar navbarState={navbarOpen} handleNavbar={handleNavbar} />
-        <GlobalStyle />
-      </Tablet> */}
       <Desktop>
         {router.pathname == "/allblogs" ? null : <LeftPanel />}
         <Component {...pageProps} />
